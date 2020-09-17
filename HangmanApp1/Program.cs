56cadd18acd70d3fdde7133413640cb631b7d0ca
gtfyds <07otaite-jupp@brightoncollege.net>
2020-09-17 14:39:27 +0100

    Initial commit

---
+++ HangmanApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DefaultNamespace
+{
+    public class Hangman
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            // Ben - enter the words and allow user to choose
+            // Ollie - function that loops through the word to see where letters are
+            // Main while loop
+        }
+    }
+
+    class LetterFunction
+    {
+        public static void WordFinder(string word)
+        {
+            Console.WriteLine("Test");
+        }
+    }
+
+} 
