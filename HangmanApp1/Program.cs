b6777cbba0a345c93b3c87b63afba4fccaccc4be
gtfyds <07otaite-jupp@brightoncollege.net>
2020-09-18 10:13:14 +0100

    Added a working function

--- HangmanApp1/Program.cs
+++ HangmanApp1/Program.cs
@@ -6,18 +6,19 @@ namespace DefaultNamespace
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Test");
             // Ben - enter the words and allow user to choose
             // Ollie - function that loops through the word to see where letters are
             // Main while loop
+            Console.WriteLine(new LetterFunction());
         }
     }
 
     class LetterFunction
     {
-        public static void WordFinder(string word)
+        public string WordFinder(string word)
         {
-            Console.WriteLine("Test");
+            string result = "Done";
+            return result;
         }
     }
 
