2d280fac7adca3d3e44be7030b9a11041cd59997
gtfyds <07otaite-jupp@brightoncollege.net>
2020-09-21 16:57:36 +0100

    Shaved it down a bit

--- HangmanApp1/Program.cs
+++ HangmanApp1/Program.cs
@@ -4,45 +4,27 @@ namespace HangmanApp1
 {
     public class Hangman
     {
-        // defining the function
-        public Array WordFinder()
+        public bool Contains(String str)
         {
-            for (int j = 0; j < mysteryWord.Length; j++)
-            {
-                if (playerGuess == mysteryWord[j])
-                    guess[j] = playerGuess;
-
-            }
-
-            return null;
+            return true;
         }
 
         public Array WordCreator()
-
-        // input
-        Console.WriteLine("Enter the word you want to guess");
-        string EnterWord = Console.ReadLine();
-        string[] LetterList = new string[EnterWord.Length];
+        {
+            // input
+            Console.WriteLine("Enter the word you want to guess");
+            string EnterWord = Console.ReadLine();
+            string[] LetterList = new string[EnterWord.Length];
+            return LetterList;
+        }
 
         public static void Main(string[] args)
         {
             // Ben           - enter the words and allow user to choose
             // Ollie TJ      - function that loops through the word to see where letters are
             // Ollie Blom    - Main while loop
-
-
-            // returning the function
-            LetterFunction lf = new LetterFunction();
-            Array Done = lf.WordFinder();
-
-            // prints the array
-            // similar to         for i in range(x,y):
-            //                        print(array[i])
-
-            foreach (var item in Done)
-            {
-                Console.WriteLine(item.ToString());
-            }
+            
+            
         }
     }
 }
