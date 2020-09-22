2975d386cdcf26d3a8128f4bf337cf47868367b5
gtfyds <07otaite-jupp@brightoncollege.net>
2020-09-22 19:38:17 +0100

    Created working function (sorry it is late)

--- HangmanApp1/Program.cs
+++ HangmanApp1/Program.cs
@@ -2,20 +2,19 @@
 
 namespace HangmanApp1
 {
-    public class Hangman
+    public static class Hangman
     {
-        public bool Contains(String str)
+        public static int Contains(string a,string b)
         {
-            return true;
-        }
+            for (int i = 0; i < (b.Length); i++)
+            {
+                if (a == b.Substring(i))
+                {
+                    return i;
+                }
+            }
 
-        public Array WordCreator()
-        {
-            // input
-            Console.WriteLine("Enter the word you want to guess");
-            string EnterWord = Console.ReadLine();
-            string[] LetterList = new string[EnterWord.Length];
-            return LetterList;
+            return -1;
         }
 
         public static void Main(string[] args)
@@ -23,7 +22,18 @@ namespace HangmanApp1
             // Ben           - enter the words and allow user to choose
             // Ollie TJ      - function that loops through the word to see where letters are
             // Ollie Blom    - Main while loop
-            
+            string letterChoice = "g";
+            string wordActual = "Begin";
+            int ret;
+            ret = Contains(letterChoice, wordActual);
+            if (ret != -1)
+            {
+                Console.WriteLine("You guessed the "+(ret+1)+" Character correctly");
+            }
+            else if (ret == -1)
+            {
+                Console.WriteLine("You guessed incorrectly");
+            }
             
         }
     }
