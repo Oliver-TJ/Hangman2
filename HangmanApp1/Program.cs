c7ce53372ea538fe2ea14b6ab667db796e5bf2af
OllieBlom <oliverbrooke@gmail.com>
2020-09-22 21:14:28 +0100

    Changed the Contains() function name to CheckGuess to avoid confusion Started on main game loop changed CheckGuess so it returns an array with 1 if the letter is in that position and -1 if it isn't commented out a lot of code so it can be compiled and tested Bruh

--- HangmanApp1/Program.cs
+++ HangmanApp1/Program.cs
@@ -1,20 +1,61 @@
 ﻿using System;
+using System.Diagnostics.PerformanceData;
 
 namespace HangmanApp1
 {
     public static class Hangman
     {
-        public static int Contains(string a,string b)
+        public static int[] CheckGuess(char a,string b)
         {
+            int[] positions = new int[b.Length];
+            bool contain = false;
             for (int i = 0; i < (b.Length); i++)
             {
-                if (a == b.Substring(i))
+                if (b[i] == a)
                 {
-                    return i;
+                    positions[i] = 1;
                 }
+                else
+                {
+                    positions[i] = -1;
+                }
+                //positions[i] = b.IndexOf(a, i, i);
             }
 
-            return -1;
+            return positions;
+        }
+
+        public static string WordEntry()
+        {
+            Console.WriteLine("Enter the word you would like someone to guess: ");
+            return Console.ReadLine();
+        }
+
+        public static void GameLoop()
+        {
+           string word = WordEntry();
+           //Console.WriteLine(word);
+           int counter = word.Length;
+           while (counter > 0)
+           {
+               Console.WriteLine("Enter your guess: ");
+               char guess = Convert.ToChar(Console.ReadLine());
+               int[] guessPositions = CheckGuess(guess, word);
+               for (int i = 0; i < (word.Length); i++)
+               {
+                   Console.WriteLine(guessPositions[i]);
+               }
+               Console.WriteLine(Convert.ToString(CheckGuess(guess, word)));
+               /*if (Contains(guess, word) == -1)
+               {
+                   Console.WriteLine("You guessed the "+(ret+1)+" Character correctly");
+               }
+               else if (ret == -1)
+               {
+                   Console.WriteLine("You guessed incorrectly");
+               }
+               */
+           }
         }
 
         public static void Main(string[] args)
@@ -24,7 +65,8 @@ namespace HangmanApp1
             // Ollie Blom    - Main while loop
             string letterChoice = "n";
             string wordActual = "Beginning";
-            int ret;
+            GameLoop();
+            /* int ret;
             ret = Contains(letterChoice, wordActual);
             if (ret != -1)
             {
@@ -34,7 +76,7 @@ namespace HangmanApp1
             {
                 Console.WriteLine("You guessed incorrectly");
             }
-            
+            */
         }
     }
 }
