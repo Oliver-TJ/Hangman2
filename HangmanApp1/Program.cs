68eba30177d2ea012f5d756d1b5c31db412fa6ad
OllieBlom <oliverbrooke@gmail.com>
2020-09-23 09:29:14 +0100

    Made it look cool

--- HangmanApp1/Program.cs
+++ HangmanApp1/Program.cs
@@ -27,8 +27,51 @@ namespace HangmanApp1
 
         public static string WordEntry()
         {
+            Console.WriteLine(
+                "     |/|\n" +
+                "     |/|\n" +
+                "     |/|\n" +
+                "     |/|\n" +
+                "     |/|\n" +
+                "     |/|\n" +
+                "     |/| /¯)\n" +
+                "     |/|/ /\n" +
+                "     |/| /\n" +
+                "    (¯¯¯)\n" +
+                "    (¯¯¯)\n" +
+                "    (¯¯¯)\n" +
+                "    (¯¯¯)\n" +
+                "    (¯¯¯)\n" +
+                "    /¯¯/\\ \n" +
+                "   / ,^./\\ \n" +
+                "  / /   \\/\\ \n" +
+                " / /     \\/\\ \n" +
+                "( (       )/)\n" +
+                "| |       |/|\n" +
+                "| |       |/|\n" +
+                "| |       |/|\n" +
+                "( (       )/)\n" +
+                " \\ \\     / /\n" +
+                "  \\ `---' /\n" +
+                "   `-----'\n" +
+                "888\n" +
+                "888\n" +
+                "888\n" +
+                "88888b.  8888b. 88888b.  .d88b. 88888b.d88b.  8888b. 88888b.\n" +
+                "888  88b     88b888  88bd88P 88b888  888  88b     88b888  88b \n" +
+                "888  888.d888888888  888888  888888  888  888.d888888888  888 \n" +
+                "888  888888  888888  888Y88b 888888  888  888888  888888  888\n" +
+                "888  888 Y888888888  888  Y88888888  888  888 Y888888888  888 \n" +
+                "                             888\n" +
+                "                        Y8b d88P\n" +
+                "                          Y88P ");
             Console.WriteLine("Enter the word you would like someone to guess: ");
             return Console.ReadLine();
+        }
+
+        public static void DrawGame()
+        {
+            
         }
 
         public static void GameLoop()
